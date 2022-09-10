fb316ae9bb7c5ad685d1546218fd50889874b9fe
kulklexs <kulklex@gmail.com>
2022-09-09 17:10:03 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+int n;
+char ch;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+for (ch = 'a'; ch<= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
