207ccabba5f00d38d86eedc9a313516000122322
kulklexs <kulklex@gmail.com>
2022-10-06 05:35:02 -0700

    fixes

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,6 @@
 #ifndef ALX
 #define ALX
-int _putchar(cahr c);
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
